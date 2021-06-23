598fb48aef8d715a8b0e86af45155b8a15f9e500
shao xian lay <shaoxianlay@shaos-MacBook-Pro.local>
2021-06-23 16:48:16 +0800

    change abnormal range

--- assst.js
+++ assst.js
@@ -2,38 +2,37 @@
 
 
 
-
-function calcl(){
-
+// step 1  
+function calcl(){              
+// step 2 
 const dis = document.getElementById('distance').value; 
 const cons = document.getElementById('fuel-used').value;
 var show = document.getElementById('display');
 
 
-// error out empty string 
+//  step 3   error out empty string 
       if ( dis == "" || cons == "" ){
           alert("pls enter all fields".toUpperCase())
   
-      }   // error out alphabetic 
+      }   //  step 4    error out alphabetic 
       else if ( isNaN(dis) || isNaN(cons)){
         alert(`pls enter number `.toUpperCase()); 
-     }   // ensure is within regular range 
-      else if (dis >= 1000 || cons >= 500){
-      show.innerHTML = `the km has exceeded the maximum limits pls re-enter`
-      } 
-      else {     //compute result 
+     }   //  step 5  ensure is within regular range 
+      else if (dis >= 1000 ){show.innerHTML = `the km has exceeded the maximum limits pls re-enter`} 
+      else if (cons >= 500){show.innerHTML = `the fuel consumed has reached abnormal range  pls re-enter`} 
+      else {     // step 6 compute result 
               let result  = ( cons / dis ) * 100;
               const toDecimal = result.toFixed(2); 
-                  // display 
+                  //  step 7 and 8   catergorize  and display display 
                 switch(toDecimal !=NaN) {
                 case   toDecimal <= 8 :
-                    show.innerHTML = `your car efficiency is ${toDecimal}  and  fuel efficient `
+                    show.innerHTML = `your car used ${toDecimal} litre  per 100km  under section of fuel efficient `
                     break
-                case toDecimal > 8 && toDecimal <= 12 :
-                  show.innerHTML = `your car efficiency is ${toDecimal}  and  average`
+                case toDecimal > 8 && toDecimal < 12 :
+                  show.innerHTML = `your car used  ${toDecimal} litre per 100km  under section of average fuel efficient  `
                     break
                 case toDecimal >= 12 :
-                  show.innerHTML = `your car efficiency is ${toDecimal}  and  bad in efficient `
+                  show.innerHTML = `your car used  ${toDecimal} litre per 100km and under section of  bad  efficient `
                     break
       }
       
